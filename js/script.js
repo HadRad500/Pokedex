6599c55d96fed35da6c515efbd1e115527b3cd14
HadRad500 <hadignasr@gmail.com>
2023-07-30 17:35:32 -0700

    Updated js with first pokemon objects

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let pokemonList = [
+    {name: 'Bulbasaur', height: 3, type: ['grass', 'poison']},
+    {name: 'Charmander', height: 6, type: ['fire', 'dragon']},
+    {name: 'Squirtle', height: 5, type: ['water', 'normal']},
+    
+]
