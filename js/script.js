c1b1fc1dc5eff51c5ff0f14bcc717fee02ff09b1
HadRad500 <hadignasr@gmail.com>
2023-08-01 09:41:03 -0700

    Added a condition for one of the pokemon

--- js/script.js
+++ js/script.js
@@ -5,9 +5,10 @@ let pokemonList = [
     
 ]
 
+
 for (let i = 0; i < pokemonList.length; i++){
-    if (pokemonList[i].height === 3) {
-      document.write('<p>Bulbasaur (height:3) </p>')
+    if (pokemonList[i].height <= 3) {
+      document.write('<p>Bulbasaur (height:3) - Oh that\'s small! </p>')
     }else if (pokemonList[i].height === 6) {
       document.write('<p>Charmander (height:6) </p>')
     } else {
