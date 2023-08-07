b54345fad48a8eccb12e6058842cae67f347754a
HadRad500 <hadignasr@gmail.com>
2023-08-07 12:30:08 -0700

    Housed the array in a IIFE

--- js/script.js
+++ js/script.js
@@ -1,13 +1,29 @@
+let pokemonRepository = (function () {
+    
 let pokemonList = [
     {name: 'Bulbasaur', height: 3, type: ['grass', 'poison']},
     {name: 'Charmander', height: 6, type: ['fire', 'dragon']},
     {name: 'Squirtle', height: 5, type: ['water', 'normal']},
-    
 ]
+
+function add(pokemon) {
+    pokemonList.push(pokemon);
+}
+
+function getAll() {
+    return pokemonList;
+}
+
+return{
+    add: add,
+    getAll: getAll
+};
+})();
 
 
   pokemonList.forEach(function(pokemon) {
     console.log(pokemon.name + 'is' + pokemon.height + 'that\'s crazy.');
+    console.log(pokemonRepository.getAll());
   });
 
 
@@ -30,4 +46,4 @@ let pokemonList = [
     }else {
       document.write(`<p>${pokemonList[i].name} (height:${pokemonList[i].height}) </p>`)
     }
-  }
+  }*/
